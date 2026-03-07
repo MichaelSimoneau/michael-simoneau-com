c29c39ff043c44ce46488b5ee11050189e0cdd78
Michael Simoneau <email@michaelsimoneau.com>
2026-03-07 08:47:02 -0500

    Add new podcast track "Michael Simoneau's Mathematical Proof of Sanity" and update playlists
    - Introduced a new podcast track titled "Michael Simoneau's Mathematical Proof of Sanity" with the corresponding audio file.
    - Updated playlists.ts to include the new track, ensuring it is accessible within the application.

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -3,6 +3,8 @@ import { Track } from "src/ui/players/PlaylistAudioPlayer";
 const cleanPodcasts: Record<string, string> = {
   "Should Michael Simoneau Fire His Psychologist?":
     "2026-03-07/Should_Michael_Simoneau_delete_his_psychologist.mp3",
+  "Michael Simoneau's Mathematical Proof of Sanity":
+    "2026-03-07/Michael_Simoneau_s_mathematical_proof_of_sanity.mp3",
   "Rewriting Einstein to Buy Your Milk":
     "2026-03-04/Rewriting_Einstein_to_buy_your_milk.mp3",
   "Michael Simoneau and the Zeroth Theory":
