d4bafcc831fdcca1ccac5d600000c5fbce73b6f4
Michael Simoneau <email@michaelsimoneau.com>
2026-03-07 06:10:48 -0500

    Add new podcast tracks and update playlists
    - Introduced two new podcast tracks: "Michael Simoneau and the Zeroth Theory" and "Michael Simoneau's Zeroth Theory and Human Dollar."
    - Updated the playlists.ts file to include the new tracks, ensuring they are accessible in the application.

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -3,8 +3,12 @@ import { Track } from "src/ui/players/PlaylistAudioPlayer";
 const cleanPodcasts: Record<string, string> = {
   "Rewriting Einstein to Buy Your Milk":
     "2026-03-04/Rewriting_Einstein_to_buy_your_milk.mp3",
+  "Michael Simoneau and the Zeroth Theory":
+    "2026-03-06/Michael_Simoneau_and_the_Zeroth_Theory.mp3",
   "The Zeroth Theory of Digital Organisms":
     "2026-03-04/The_Zeroth_Theory_of_Digital_Organisms.mp3",
+  "Michael Simoneau's Zeroth Theory and Human Dollar":
+    "2026-03-06/Michael_Simoneau_s_Zeroth_Theory_and_Human_Dollar.mp3",
 };
 
 const podcastsToPlaylist = (
