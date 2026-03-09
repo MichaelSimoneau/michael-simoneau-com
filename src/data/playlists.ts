2e0f13da4339ad1b757d63d89c5387c139d1b4b6
Michael Simoneau <email@michaelsimoneau.com>
2026-03-09 05:43:25 -0400

    Update playlists.ts to include additional podcast tracks and enhance organization
    - Added new podcast tracks with corresponding dates and file paths for better content management.
    - Included comments for each track to improve clarity and maintainability of the playlist structure.
    - Organized existing tracks to ensure a consistent format and facilitate future updates.

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -2,21 +2,34 @@ import { Track } from "src/ui/players/PlaylistAudioPlayer";
 
 const cleanPodcasts: Record<string, string> = {
   "COLLAPSED_0": "Is Your Name Melinda Francis? If Not, Move Along... Nothing To See Here.",
+  // --- COLLAPSED_0 ---
+  // Track #1 - 2026-03-07
   "Should Michael Simoneau Fire His Psychologist?":
   "2026-03-07/Should_Michael_Simoneau_delete_his_psychologist.mp3",
+  // Track #2 - 2026-03-07
   "Michael Simoneau's Mathematical Proof of Sanity":
   "2026-03-07/Michael_Simoneau_s_mathematical_proof_of_sanity.mp3",
+  // Track #3 - 2026-03-04
   "Rewriting Einstein to Buy Your Milk":
   "2026-03-04/Rewriting_Einstein_to_buy_your_milk.mp3",
+  // Track #4 - 2026-03-08
   "Michael's Sunday Morning Brief: Sunday, March 8, 2026. Good Morning, Michael Simoneau...":
   "2026-03-08/Prompt_for-Why_Michael_hung_up_on_Melinda_and_why_she_should_schedule_March_19_and_why_Michael_could_not_care_less_either_way.mp3",
+  // Track #5 - 2026-03-08
   "Why Michael Hung-Up on Melinda and Why She Should Schedule March 19 and Why Michael Could Not Care Less Either Way...":
   "2026-03-08/Why_Michael_hung_up_on_Melinda_and_why_she_should_schedule_March_19_and_why_Michael_could_not_care_less_either_way.mp3",
+  // --- EXPANDED_1 ---
   "EXPANDED_1": "Our New Reality: Rewriting Einstein to Fix the Global Economy",
+  // Track #6 - 2026-03-09
+  "The Architect and his Autonomous Digital Organism":
+    "2026-03-09/The_Architect_and_his_autonomous_digital_organism.mp3",
+  // Track #7 - 2026-03-06
   "Michael Simoneau and the Zeroth Theory":
     "2026-03-06/Michael_Simoneau_and_the_Zeroth_Theory.mp3",
+  // Track #8 - 2026-03-04
   "The Zeroth Theory of Digital Organisms":
     "2026-03-04/The_Zeroth_Theory_of_Digital_Organisms.mp3",
+  // Track #9 - 2026-03-06
   "Michael Simoneau's Zeroth Theory and Human Dollar":
     "2026-03-06/Michael_Simoneau_s_Zeroth_Theory_and_Human_Dollar.mp3",
 };
