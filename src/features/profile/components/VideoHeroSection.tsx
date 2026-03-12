4254d52ce6d5eb8db4cdf7645eacede0a61360e0
Michael Simoneau <email@michaelsimoneau.com>
2026-03-12 08:40:52 -0400

    Update VideoHeroSection to change URL hash on reload
    - Modified the URL handling in VideoHeroSection to set the hash to '#about' instead of redirecting to the root.
    - Enhanced session storage management to maintain state during user interactions.

--- src/features/profile/components/VideoHeroSection.tsx
+++ src/features/profile/components/VideoHeroSection.tsx
@@ -178,9 +178,8 @@ export const VideoHeroSection: React.FC = () => {
     flowDispatch({ type: 'RELOAD_TIMER_COMPLETED' });
     window.sessionStorage.setItem(MELINDA_RELOAD_GUARD_KEY, '1');
     const nextUrl = new URL(window.location.href);
-    nextUrl.pathname = '/';
     nextUrl.searchParams.set('_', Date.now().toString());
-    nextUrl.hash = '';
+    nextUrl.hash = '#about';
     window.location.assign(nextUrl.toString());
     return true;
   }, [flowDispatch, isBeforeNoon, isRestrictedOverlayLocked]);
