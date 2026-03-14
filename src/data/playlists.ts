641f87185483b021a368d7ec3693c85ebdec8569
Michael Simoneau <email@michaelsimoneau.com>
2026-03-14 05:18:51 -0400

    Update music playlist with track replacements in playlists.ts
    - Swapped the positions of tracks in the musicPlaylist, replacing "She's a Freak" with "I'm In Deep" and vice versa, ensuring the playlist reflects the correct audio content for users.

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -159,11 +159,11 @@ const melindaFrancisPlaylist = podcastsToPlaylist({
 
 const musicPlaylist = podcastsToPlaylist({
   // Track #1
-  '"She\'s a Freak" - Mike Crane': "/music/ShesAFreak.mp3",
-  // Track #2
-  '"This Is Why We Do It" - Mike Crane': "/music/ThisIsWhyWeDoIt.mp3",
-  // Track #3
   '"I\'m In Deep" - Mike Crane': "/music/ImInDeep.mp3",
+  // Track #2
+  '"She\'s a Freak" - Mike Crane': "/music/ShesAFreak.mp3",
+  // Track #3
+  '"This Is Why We Do It" - Mike Crane': "/music/ThisIsWhyWeDoIt.mp3",
   // Track #4
   '"The Drugs Work" - Mike Crane': "/music/TheDrugsWork.m4a",
 });
