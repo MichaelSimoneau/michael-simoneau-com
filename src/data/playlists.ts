c1c1186ca9b6cad2266a7a95328d5ae0f11000d9
Michael Simoneau <email@michaelsimoneau.com>
2026-03-15 14:18:13 -0400

    Add new audio files and transcripts for Chardon Rd and Google's Latest AI Analysis
    - Introduced new audio file for "Chardon Rd" along with its corresponding transcript.
    - Added audio file for "Google's Latest AI Analyzed Michael Simoneau's Behavior" and its transcript, enhancing the audio content library.
    - Ensured proper file structure and naming conventions for better organization and accessibility.

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -141,7 +141,7 @@ const melindaFrancisPlaylist = podcastsToPlaylist({
   "Michael Simoneau Cured His Sanity by Diagnosing Everyone Else":
     "/audio/2026-03-14/Why_Michael_Simoneau_engineered_expiring_money.mp3",
   EXPANDED_1: "AI Exposes Michael Simoneau's Behavior",
-  "Google's Latest AI Exposes Michael Simoneau's Behavior": "/audio/2026-03-15/Google's_Latest_AI_Analyzed_Michael_Simoneau's_Behavior.mp3",
+  "Google's Latest AI Exposes Michael Simoneau's Behavior": "/audio/2026-03-15/Google_s_Latest_AI_Analyzed_Michael_Simoneau_s_Behavior.mp3",
   COLLAPSE_0: "How Michael Simoneau is 'The Living Loophole'...",
   "...and How He Trapped Melinda Francis":
     "/audio/2026-03-13/Zero-Sudoku-Implementation.mp3",
